38e3428b6909c26e787678c728fd0603cfe33db2
Muasa-harman <harman.muasa@gmail.com>
2025-08-23 07:23:08 +0300

    sign up

--- backend/Program.cs
+++ backend/Program.cs
@@ -81,17 +81,3 @@ app.MapControllers();
 app.Run();
 
 
-
-// using Microsoft.AspNetCore.Hosting;
-// using Microsoft.AspNetCore.Builder;
-// using Microsoft.AspNetCore.Http;
-// using Microsoft.Extensions.Hosting;
-
-// WebApplication app = WebApplication.Create();
-// app.Run(async context =>
-// {
-//     // Duplicate the code below and write more messages. Save and refresh your browser to see the result.
-//     await context.Response.WriteAsync("Hello world .NET 8. Make sure you run this app using 'dotnet watch run'.");
-// });
-
-// await app.RunAsync();
